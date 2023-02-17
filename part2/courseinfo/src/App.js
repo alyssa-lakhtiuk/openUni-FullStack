7ffed4b90ab448986c8d0a0bbe4343c2182d4f85
Аліса Лахтюк <alisalahtiuk@gmail.com>
2023-02-18 00:14:56 +0200

    2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -2,7 +2,6 @@ const Header = ({ course }) => <h1>{course}</h1>
 
 const Total = ({ parts }) => {
   const sum = parts.reduce((total, item) => total = total + item.exercises, 0)
-  //const sum= parts.reduce((a,v) =>  a = a + v.exercises , 0 )
   return <div>
       <b>total {sum} of exercises</b>
     </div>
