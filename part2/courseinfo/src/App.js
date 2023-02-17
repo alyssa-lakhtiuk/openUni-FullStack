db1681f8387af1029b109a7b258651f48d0cf24a
Аліса Лахтюк <alisalahtiuk@gmail.com>
2023-02-18 00:17:39 +0200

    2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,21 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((total, item) => total = total + item.exercises, 0)
-  return <div>
-      <b>total {sum} of exercises</b>
-    </div>
-}
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => <div>
-{parts.map(part => (<Part key = {part.id} part={part} />))}
-</div>
-  
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
@@ -68,15 +51,5 @@ const App = () => {
     </div>)
 }
 
-
-const Course=({course})=>{
-  return(
-    <div>
-      <Header course={course.name} />
-      <Content  parts={course.parts} /> 
-      <Total parts = {course.parts}/>
-    </div>
-  )
-}
 
 export default App
