964d79cb20999270b6af41928726b4cc1d947d61
Аліса Лахтюк <alisalahtiuk@gmail.com>
2023-02-17 18:27:43 +0200

    1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,9 +11,9 @@ const App = () => {
   return (
     <div>
       <h1>{pageHeader}</h1>
-      <button onClick={() => setGood(good+1)}>good</button>
-      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
-      <button onClick={() => setBad(bad+1)}>bad</button>
+      <Button handleClick={() => setGood(good+1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral+1)} text="neutral" />
+      <Button handleClick={() => setBad(bad+1)} text="bad" />
       <Statistics good = {good} neutral={neutral} bad={bad}/>
     </div>
   )
@@ -39,13 +39,26 @@ const Statistics = ({good, neutral, bad}) => {
 
   return <div> 
     <h1> {pageHeader2} </h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} % </p>
+    <StatisticLine text="good" value = {good}/>
+    <StatisticLine text="neutral" value = {neutral}/>
+    <StatisticLine text="bad" value = {bad}/>
+    <StatisticLine text="all" value = {all}/>
+    <StatisticLine text="average" value = {average}/>
+    <StatisticLine text="positive" value = {positive}/>
   </div>
+}
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const StatisticLine = ({text, value}) => {
+  if (text === "positive") {
+    return <div><p> {text} {value} %</p></div>
+  }
+  return <div><p> {text} {value}</p></div>
 }
 
 export default App
