f9a797d045b409b62af8df9d2cd38f323952cf91
Аліса Лахтюк <alisalahtiuk@gmail.com>
2023-02-18 16:06:35 +0200

    2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,36 +2,53 @@ import {useState} from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    { name: 'Arto Hellas', 
+      number : '040-1234567'}
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
-  const addNewName = (event) => {
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const addNewNameNNumber = (event) => {
     event.preventDefault()
     const nameObject = {
-      name : newName
+      name : newName,
+      number : newNumber
     }
     if(persons.some(item => item.name === newName) === true){
-      console.log("alert must be called")
+      console.log("alert for name called")
       alert(`${newName} is already added to phonebook`)
+      return
+    }
+    if(persons.some(item => item.number === newNumber) === true){
+      console.log("alert for number called")
+      alert(`${newNumber} is already added to phonebook`)
       return
     }
     console.log(persons)
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNewName}>
+      <form onSubmit={addNewNameNNumber}>
         <div>
           name : <input value = {newName} onChange = {handleNameChange}/>
+        </div>
+        <div>
+          number : <input value = {newNumber} onChange = {handleNumberChange}/>
         </div>
         <div>
           <button type = "submit">add</button>
@@ -43,6 +60,6 @@ const App = () => {
   )
 } 
 
-const Person = ({person}) => <div>{person.name}</div>
+const Person = ({person}) => <div>{person.name} {person.number}</div>
 
 export default App
