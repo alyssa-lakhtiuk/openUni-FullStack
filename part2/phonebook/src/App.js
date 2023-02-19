66b33a127e05a8b06fd733b7e6b221249efaf396
Аліса Лахтюк <alisalahtiuk@gmail.com>
2023-02-19 12:47:28 +0200

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,11 +2,14 @@ import {useState} from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', 
-      number : '040-1234567'}
+    { name: 'Arto Hellas', number : '040-1234567', id: 1}, 
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterValue, setFilterValue] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -18,11 +21,24 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFiltration = (event) => {
+    console.log(event.target.value)
+    setFilterValue(event.target.value)
+  }
+
+  const filteredPersons = persons
+    .filter((person) =>
+      person.name.toLowerCase().includes(filterValue.toLowerCase())
+    )
+    
+  
+
   const addNewNameNNumber = (event) => {
     event.preventDefault()
     const nameObject = {
       name : newName,
-      number : newNumber
+      number : newNumber,
+      id: persons.length + 1
     }
     if(persons.some(item => item.name === newName) === true){
       console.log("alert for name called")
@@ -40,9 +56,15 @@ const App = () => {
     setNewNumber('')
   }
 
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with : <input value = {filterValue} onChange = {handleFiltration}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNewNameNNumber}>
         <div>
           name : <input value = {newName} onChange = {handleNameChange}/>
@@ -55,7 +77,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{persons.map(person => <Person key={person.name} person={person}></Person>)}</div>
+      <div>{filteredPersons.map(person => <Person key={person.id} person={person}></Person>)}</div>
     </div>
   )
 } 
