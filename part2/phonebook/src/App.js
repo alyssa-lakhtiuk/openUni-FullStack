9ec788b4e4e3e78c913eca6c9efd11c88e469da2
Аліса Лахтюк <alisalahtiuk@gmail.com>
2023-02-21 23:27:05 +0200

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,25 @@
-import {useState} from 'react'
+import React from 'react';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number : '040-1234567', id: 1}, 
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', number : '040-1234567', id: 1}, 
+  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  // ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilterValue] = useState('')
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
+  
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
@@ -22,7 +31,6 @@ const App = () => {
   }
 
   const handleFiltration = (event) => {
-    console.log(event.target.value)
     setFilterValue(event.target.value)
   }
 
@@ -31,7 +39,6 @@ const App = () => {
       person.name.toLowerCase().includes(filterValue.toLowerCase())
     )
     
-  
 
   const addNewNameNNumber = (event) => {
     event.preventDefault()
