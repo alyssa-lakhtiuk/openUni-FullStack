9c6c61bfc643244004c55c23bce065a304adff1c
Аліса Лахтюк <alisalahtiuk@gmail.com>
2023-03-03 20:51:22 +0200

    2.20

--- part2/countries/src/services/weather.js
+++ part2/countries/src/services/weather.js
@@ -4,7 +4,6 @@ import {useEffect, useState} from 'react'
 const api_key = process.env.REACT_APP_API_KEY
 
 const getWeather = (country) => {
-    console.log(api_key)
     const lat = country.capitalInfo.latlng[0]
     const lng = country.capitalInfo.latlng[1]
 
